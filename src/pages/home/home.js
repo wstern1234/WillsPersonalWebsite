880eb99ce95735560ded59385d7921853600a3bb
Wills Stern <willsstern@gmail.com>
2021-05-26 20:37:24 -0700

    update interests

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -45,8 +45,8 @@ const Home = (props) => {
           margin = '-40px'>
             Hello! My name is Wills Stern. I'm a junior at San Ramon Valley High School and working 
             on my Associate Degree in Computer Science from Diablo Valley College. Currently, I'm on 
-            track to receive both by Spring 2022. My interests include programming, AI/ML, Uno R3 
-            automation, and learning new technologies.
+            track to receive both by Spring 2022. My interests include AI Assistants, aerospace engineering, 
+            nuclear/particle physics and learning new technologies.
           </Text>
         </Box>
 
